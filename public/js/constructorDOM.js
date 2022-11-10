1e4bc05b458b9e163d9c9de1fd89f2ac01096588
Andreas S <andyschima@gmail.com>
2022-11-10 12:28:51 +0100

    application du style debut

--- public/js/constructorDOM.js
+++ public/js/constructorDOM.js
@@ -15,6 +15,11 @@ export function divConstructor(character) {
     newA.textContent = "See Character";
     newA.href = `../public/htmlPages/singleCharacter.html?id=${character.id}`;
     newA.target = "_blank";
+    newDiv.classList.add("font-marvel", "flex", "flex-col", "bg-slate-900", "rounded", "p-5", "shadow-lg", "shadow-cyan-700");
+    newImg.classList.add("h-24", "m-1.5", "max-w-min", "mr-auto", "ml-auto", "shadow-xl", "shadow-cyan-700", "rounded-full");
+    newH.classList.add("m-2", "mt-5", "text-left", "text-white");
+    newDesc.classList.add("m-2", "text-white");
+    newA.classList.add("animate-bounce", "w-2/4", "m-2", "mt-5", "text-center", "bg-gradient-to-r", "from-green-500", "to-blue-500", "hover:from-pink-500", "hover:to-yellow-500", "text-white", "rounded-lg");
     newDiv.appendChild(newImg);
     newDiv.appendChild(newH);
     newDiv.appendChild(newDesc);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./main.js", "./public/htmlPages/createCharacter.html", "./public/htmlPages/singleCharacter.html"],
+  content: [
+              "./index.html",
+              "./main.js",
+              "./public/htmlPages/createCharacter.html",
+              "./public/htmlPages/singleCharacter.html",
+              "./public/htmlPages/modifyCharacter.html",
+              "./public/htmlPages/modifyCharacter.html",
+              "./public/js/apiRequests.js",
+              "./public/js/constructorDOM.js",
+              "./public/js/createCharacter.js",
+              "./public/js/modifyCharacter.js",
+              "./public/js/singleCharacter.js"
+            ],
   theme: {
     extend: {   
       backgroundColor: theme => ({
