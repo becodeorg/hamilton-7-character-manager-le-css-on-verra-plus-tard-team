572aac5eae7c841c6294b8e498bb42ad0281b0c7
Andreas S <andyschima@gmail.com>
2022-11-14 10:56:28 +0100

    fini js create character

--- public/js/createCharacter.js
+++ public/js/createCharacter.js
@@ -1,12 +1,29 @@
 import { sendLetterAsync } from "./apiRequests"
 
+   
+const imgInput = document.querySelector("#image");
+let image = imgInput.addEventListener('change', () => {
+  console.log(imgInput.files);
+  const preview = document.querySelector("#preview");
+  let newImgURL = imgInput.files[0];
+  let newImg = document.createElement("img");
+  newImg.src = URL.createObjectURL(newImgURL);
+  preview.appendChild(newImg);
+  let reader = new FileReader();
+  reader.onloadend = () => {
+    console.log(reader.result);
+    image = reader.result.replace('data:', '').replace(/^.+,/, '');
+  };
+  reader.readAsDataURL(newImgURL);
+});
+
 let createCharBtn = document.querySelector("#createBtn");
 createCharBtn.addEventListener("click",async () => {
     let jsonLetter = {
         description: document.querySelector("#description").value,
         shortDescription: document.querySelector("#shortDescription").value,
         name: document.querySelector("#name").value,
-        image : document.querySelector("#urlImg").value
+        image : image
     }
     let response = await sendLetterAsync(jsonLetter);
     console.log(response.id);
