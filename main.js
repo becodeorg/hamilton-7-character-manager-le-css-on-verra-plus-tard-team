dd7e97cf950b0da597916f72e7abfa1e72f20753
Andreas S <andyschima@gmail.com>
2022-11-15 14:16:21 +0100

    barre de recherche

--- main.js
+++ main.js
@@ -5,6 +5,26 @@ import { divConstructor } from "./public/js/constructorDOM"
 import { goFetchAllAsync } from "./public/js/apiRequests"
 import axios from "axios"
 
+function characterSearch() {
+  let input = document.getElementById("search");
+  let name = "";
+  let searchWord = input.value.toUpperCase();
+  let cardContainer = document.getElementById("cardContainer");
+  let cards = cardContainer.querySelectorAll("#card");
+  for (let i = 0; i < cards.length; i++) {
+    let nameTag = cards[i].querySelector("h2");
+    console.log(nameTag.innerText);
+    name = nameTag.textContent || nameTag.innerText;
+    console.log(name);
+    if (name.toUpperCase().indexOf(searchWord) > -1) {
+      cards[i].style.display = "";
+    }else{
+      cards[i].style.display = "none";
+    }
+  }
+
+}
+
 const justGiveMeAFreakingFunctionSiVouPlait = async() =>{
   let charList = await goFetchAllAsync();
 
@@ -16,3 +36,4 @@ const justGiveMeAFreakingFunctionSiVouPlait = async() =>{
 
 
 justGiveMeAFreakingFunctionSiVouPlait()
+document.getElementById("search").addEventListener("keyup", characterSearch);
